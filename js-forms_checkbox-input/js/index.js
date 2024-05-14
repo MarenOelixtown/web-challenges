1ed8b10475ec3b51c0bf024151baf56ee2d7c4b8
Maren <marenehlers@gmx.net>
2024-05-14 20:21:44 +0200

    update solution

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -11,6 +12,14 @@ function hideTosError() {
 function showTosError() {
   tosError.removeAttribute("hidden");
 }
+
+function hideSuccessMessage() {
+  successMessage.setAttribute("hidden", "");
+}
+function showSuccessMessage() {
+  successMessage.removeAttribute("hidden");
+}
+
 tosCheckbox.addEventListener("input", (event) => {
   if (event.target.checked === true) {
     hideTosError();
@@ -35,5 +44,6 @@ form.addEventListener("submit", (event) => {
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  showSuccessMessage();
+  /*  alert("Form submitted"); */
 });
