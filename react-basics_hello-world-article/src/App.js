ad48586938ad4c501129e5b6457aca9bfbf6936a
Maren <marenehlers@gmx.net>
2024-05-27 16:59:12 +0200

    update solution

--- react-basics_hello-world-article/src/App.js
+++ react-basics_hello-world-article/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return HelloWorldArticle();
+  return <HelloWorldArticle />;
 }
 
 function HelloWorldArticle() {
   return (
-    <div>
+    <article>
       <h1>Hello World!</h1>
       <p>I have written a headline and a paragraph with REACT.</p>
-    </div>
+    </article>
   );
 }
