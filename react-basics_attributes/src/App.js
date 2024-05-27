c014ae2bd8c4dd038f557ff0c54b2909e94794c9
Maren <marenehlers@gmx.net>
2024-05-27 16:42:54 +0200

    upload solution

---
+++ react-basics_attributes/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return Article();
+}
+
+function Article() {
+  return (
+    <article>
+      <h2 className="article__title">Hello World!</h2>
+      <label htmlFor="input-test">This is a test-input: </label>
+      <input type="text" id="input-test"></input>
+      <a className="article__link" href="https://de.wikipedia.org/wiki/Hamburg">
+        Do you want to know more about Hamburg?
+      </a>
+    </article>
+  );
+}
