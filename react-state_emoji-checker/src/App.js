d07f2802f6efd1ee295c2953f6c74fc3809c5ef8
Maren <marenehlers@gmx.net>
2024-05-29 13:26:10 +0200

    upload solution

---
+++ react-state_emoji-checker/src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [code, setCode] = useState("");
+
+  const validCode = "🐡🐠🐋";
+
+  // Arrow-Function
+  /* const handleClick = (event) => {
+    console.log(code);
+    setCode(code + event.target.textContent);
+  }; */
+
+  function handleClick(event) {
+    console.log(code);
+    console.log(event.target.textContent);
+    return setCode(code + event.target.textContent);
+  }
+
+  return (
+    <div className="container">
+      <div className="button-container">
+        <button
+          type="button"
+          onClick={(event) => {
+            handleClick(event);
+          }} // also possible onClick={handleClick}
+        >
+          <span role="img" aria-label="Pufferfish">
+            🐡
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={(event) => {
+            handleClick(event);
+          }}
+        >
+          <span role="img" aria-label="Whale">
+            🐋
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={(event) => {
+            handleClick(event);
+          }}
+        >
+          <span role="img" aria-label="Clownfish">
+            🐠
+          </span>
+        </button>{" "}
+      </div>
+
+      <button
+        type="button"
+        onClick={() => {
+          setCode("");
+        }}
+      >
+        Reset
+      </button>
+
+      {code === validCode && <p>Valid code!</p>}
+    </div>
+  );
+}
